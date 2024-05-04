2d0fe1854616086b16d6b459b68e703ef41d47f2
woxane <mohsen.minavand1384@gmail.com>
2024-05-04 17:14:14 +0330

    implement functions that needed' '

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -11,38 +11,55 @@ public class Parser {
 
     public List<Country> sortByName(){
         List<Country> sortedByName = new ArrayList<>(countries);
-        // Sort countries alphabetically (least)
-        //TODO
+        Collections.sort(sortedByName, new Comparator<Country>() {
+            @Override
+            public int compare(Country country1, Country country2) {
+                return country1.getName().compareTo(country2.getName());
+            }
+        });
+
         return  sortedByName;
     }
 
     public List<Country> sortByPopulation(){
         List<Country> sortedByPopulation = new ArrayList<>(countries);
-        // Sort countries by population (most)
-        //TODO
+        Collections.sort(sortedByPopulation, new Comparator<Country>() {
+            @Override
+            public int compare(Country country1, Country country2) {
+                return Integer.compare( country1.getPopulation(),country2.getPopulation());
+            }
+        });
         return sortedByPopulation;
     }
 
     public List<Country> sortByArea(){
         List<Country> sortedByArea = new ArrayList<>(countries);
-        // Sort countries by area (most)
-        //TODO
+        Collections.sort(sortedByArea, new Comparator<Country>() {
+            @Override
+            public int compare(Country country1, Country country2) {
+                return Double.compare( country1.getArea(),country2.getArea());
+            }
+        });
         return sortedByArea;
     }
 
     public void setUp() throws IOException {
+        Document doc = Jsoup.parse(new File("src/Resources/country-list.html"), "UTF-8");
+        Elements countryDivs = doc.select("div.country");
+        countries = new ArrayList<>();
+        for(Element div : countryDivs)
+        {
+            Element countryName = div.select("h3.country-name").first();
+            Element countryPopulation = div.select("div.country-info span.country-population").first();
 
-        //Parse the HTML file using Jsoup
-        //TODO
+            Element countryCapital = div.select("div.country-info span.country-capital").first();
 
-        // Extract data from the HTML
-        //TODO
+            Element countryArea = div.select("div.country-info span.country-area").first();
 
-        // Iterate through each country div to extract country data
-        //TODO
+            Country country = new Country(countryName.text(),countryCapital.text(), Integer.parseInt(countryPopulation.text()), Double.parseDouble(countryArea.text()));
+            countries.add(country);
+        }
     }
 
     public static void main(String[] args) {
-        //you can test your code here before you run the unit tests ;)
-    }
 }
